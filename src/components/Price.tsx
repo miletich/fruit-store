73af6f3db2ac00117e8967094797db09a8df143f
Dušan Miletić <d@miletich.cc>
2024-01-25 22:05:57 +0100

    add Price

---
+++ src/components/Price.tsx
@@ -0,0 +1,16 @@
+import { twMerge } from 'tailwind-merge';
+
+import P from './P';
+
+type Props = {
+  price: string | number;
+  className?: string;
+};
+
+export default function Price({ price, className }: Props) {
+  return (
+    <P
+      className={twMerge('text-green-600 font-semibold text-lg', className)}
+    >{`$${price}`}</P>
+  );
+}
