e24238bfd29050b761e46f47799c58e7ae3d0169
Dušan Miletić <d@miletich.cc>
2024-01-26 14:35:55 +0100

    add Popover

---
+++ src/components/Popover.tsx
@@ -0,0 +1,24 @@
+import * as Popover from '@radix-ui/react-popover';
+import { PropsWithChildren, forwardRef } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+export const Root = Popover.Root;
+export const Portal = Popover.Portal;
+export const Trigger = Popover.Trigger;
+export const Arrow = Popover.Arrow;
+export const Close = Popover.Close;
+
+type Props = PropsWithChildren<{ className?: string }>;
+export const Content = forwardRef<HTMLDivElement, Props>(function Content(
+  { children, className },
+  ref
+) {
+  return (
+    <Popover.Content
+      ref={ref}
+      className={twMerge('bg-purple-950 p-3 rounded-lg', className)}
+    >
+      {children}
+    </Popover.Content>
+  );
+});
